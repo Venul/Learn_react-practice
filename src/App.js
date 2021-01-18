5da95c90c1633cb312b112c566bc25a86e82b973
Lunev Sergey <s.lunev88@gmail.com>
2021-01-18 22:53:12 +0300

    2.0 start lessons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Car from './Car/Car'
 
-class App extends Component {
-  render() {
-    const divStyle = {
-      textAlign: 'center'
-    }
-
-    return (
-      <div style={divStyle}>
-        <h1>Hello world!</h1>
-
-        <Car />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
