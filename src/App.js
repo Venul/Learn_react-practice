bdff45d5ca59817ef8b9688fc79c4afc7504710c
Lunev Sergey <s.lunev88@gmail.com>
2021-01-23 00:02:53 +0300

    2.4 add new component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-// import logo from './logo.svg';
-import React from 'react';
 import './App.css';
+import Car from './Car/Car'
 
 function App() {
-  // return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-    return React.createElement (
-      'div',
-      {
-        className: "App-link"
-      },
-      React.createElement (
-      'h1',
-      null,
-      'Hell world'
-      )
-    )
-  // );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <body>
+      <p> here</p>
+        <Car />
+      </body>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,21 @@
+
+// function Car() {
+//   return (
+//     <h2>This is car component</h2>
+//   )
+// }
+
+
+// const car = () => {
+//   return (
+//     <h2>This is car component</h2>
+//   )
+// }
+
+// const car = () => <h2>This is car component</h2>
+
+// export default car;
+
+export default () => (
+    <h2>This is car component</h2>
+)
