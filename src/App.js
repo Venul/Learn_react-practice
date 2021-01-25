5eaa2ceaa58df27b8b0e7b476cc0a45282c99496
Lunev Sergey <s.lunev88@gmail.com>
2021-01-25 20:46:08 +0300

    3.1: state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
+import React, {Component} from 'react';
 import './App.css';
 import Car from './Car/Car'
 
-function App() {
+
+
+class App extends Component {
+  // нужно наследование от компоненты. Без этого стейт не работает
+
+  state = {
+    cars: [
+      {name: 'Ford', year: '2018'},
+      {name: 'Audi', year: '2016'},
+      {name: 'Mazda', year: '2010'}
+    ],
+    pageTitle: 'Cars list'
+  }
+
+render(){
+  const divStyle = {
+    textAlign: 'center'
+  }
+
+  let cars; 
+  cars = this.state.cars;
+  // не как в уроке. Пришлось создать спера переменную
+
   return (
     <div className="App">
       <header>
-        <h1>Hell world!</h1>
+        <h1>{this.state.pageTitle}</h1>
       </header>
-      <body>
-        <Car name ={'FORD'} year={2018} />
-        <Car name ={'Nissan'} year={2016}>
-          <p style={{color:'red'}}>Color</p>
-        </Car>
+      <body style={divStyle}>
+        <Car name ={cars[0].name} year={cars[0].year} />
+        <Car name ={cars[1].name} year={cars[1].year} />
+        <Car name ={cars[2].name} year={cars[2].year} />
       </body>
     </div>
   );
 }
-
+}
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -2,6 +2,5 @@ export default (props) => (
   <div>
     <h2>Car name: {props.name}</h2>
     <strong>Year: {props.year}</strong>
-    {props.children}
   </div>
 )
